428ad37d40b7918d93e828a165a18b9685f463e6
julie-devos <julie.devos@epitech.eu>
2020-12-08 16:13:48 +0100

    initial commit

---
+++ exo1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 1-Partie 5</title>
+</head>
+<body>
+<?php
+$months=array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre");
+?>
+
+</body>
+</html>
