cc2494833acd3054ea51f604f10ff6a7ff018403
julie-devos <julie.devos@epitech.eu>
2020-12-09 12:19:02 +0100

    initial commit

--- exo1.php
+++ exo1.php
@@ -7,7 +7,11 @@
 <body>
 <?php
 $months=array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre");
-?>
+    for ($i=0; $i<12;$i++){
+    ?>
+
+<p><?=$months[$i]; }?></p>
+
 
 </body>
 </html>
