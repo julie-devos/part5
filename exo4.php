4f66c47a140fc5d0c672c6cd79606db217fb2f73
julie-devos <julie.devos@epitech.eu>
2020-12-09 12:43:34 +0100

    initial commit

--- exo4.php
+++ exo4.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <meta charset="utf-8">
     <title>Exercice 4-Partie 5</title>
 </head>
 <body>
 <?php
 $months=array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
+for ($i=0;$i<12;$i++) {
 ?>
+<p><?=$months[$i];}?></p>
 
+<?php
+$months[7]='aout';
+for ($i=0;$i<12;$i++){?>
+
+<p><?=$months[$i];}?></p>
 
 
 </body>
