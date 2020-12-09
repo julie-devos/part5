10b078d4d012b832acc5cec6671ece4989eca678
julie-devos <julie.devos@epitech.eu>
2020-12-09 12:53:52 +0100

    initial commit

---
+++ exo5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 5 - Partie 5</title>
+</head>
+<body>
+
+<?php
+$hautsDeFrance=array(59 =>"Nord", 62 =>"Pas-de-Calais", 80 =>"Somme", 02 =>"Aisne", 60 => "Oise");
+foreach ($hautsDeFrance as $key => $value) { ?>
+
+<p><?= "$key : $value"; }?></p>
+
+</body>
+</html>
